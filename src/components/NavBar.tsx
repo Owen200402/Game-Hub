104b52fb0962356d44d49abbf703d14722631447
Owen Zheng <johndoe@example.com>
2024-08-06 16:29:09 -0700

    Fix the nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -7,8 +8,10 @@ const NavBar = () => {
   return (
     //   Horizontal Stack
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
-      <SearchInput></SearchInput>
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover"></Image>
+      </Link>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
